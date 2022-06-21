43562a3a29a35b2bae0e9f177c575dccec9354d2
GalinaCh04 <103421020+GalinaCh04@users.noreply.github.com>
2022-06-21 16:58:49 +0300

    Add files via upload

---
+++ ProductTypeLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zad2_project.Data;
+
+namespace zad2_project.Business
+{
+    public class ProductTypeLogic
+    {
+        private ProductsContext productsContext = new ProductsContext();
+        public List<ProductType> GetTypes()
+        {
+            return productsContext.GetType.ToList();
+        }
+
+        public string GetTypeById(int id)
+        {
+            return productsContext.GetType.ToList();
+        }
+
+        public string GetTypeById(int id)
+        {
+            return productsContext.GetType.Find(id).name_na_product;
+        }
+    }
+}
